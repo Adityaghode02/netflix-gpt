adb2f94b86076081faad32d6b64ce56c474348ac
Adityaghode02 <adityaghode92@gmail.com>
2024-07-02 15:29:24 +0530

    Deploy changes 1

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -2,25 +2,32 @@ import { useSelector } from "react-redux";
 import VideoTitle from "./VideoTitle";
 import VideoBackground from "./VideoBackground";
 
-const MainContainer = ()=>{
+const MainContainer = () => {
+    const movies = useSelector((store) => store?.movies?.nowPlayingMovies);
 
-    const movies = useSelector((store)=>store?.movies?.nowPlayingMovies);
-
-    if(movies === null) return ;
+    if (!movies || movies.length === 0) {
+        // Handle the case where movies is undefined, null, or an empty array
+        return <div className="text-white">No movies available</div>;
+    }
 
     const mainMovie = movies[0];
-    // console.log("mainMovie");
-    // console.log(mainMovie);//it is an object mainmovie
 
-    const {original_title , overview , id} = mainMovie;
-    return <div className="md:p-0 pt-[45%] bg-black">  
-        
-        <VideoTitle title={original_title} overview={overview}/>
-        <div > <VideoBackground movieId={id}/></div>
+    const { original_title, overview, id } = mainMovie || {};
 
-    </div>
-    
+    return (
+        <div className="md:p-0 pt-[45%] bg-black">
+            {original_title && overview && id ? (
+                <>
+                    <VideoTitle title={original_title} overview={overview} />
+                    <div>
+                        <VideoBackground movieId={id} />
+                    </div>
+                </>
+            ) : (
+                <div className="text-white">Movie details are missing</div>
+            )}
+        </div>
+    );
+};
 
-}
-
-export default MainContainer; 
+export default MainContainer;
