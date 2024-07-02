8c1ad5fc642ca6166a721aa19a51c8e82a130c41
Adityaghode02 <adityaghode92@gmail.com>
2024-07-02 15:55:08 +0530

    Deploy changes 2

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useSelector } from "react-redux";
 import VideoTitle from "./VideoTitle";
 import VideoBackground from "./VideoBackground";
@@ -5,14 +6,11 @@ import VideoBackground from "./VideoBackground";
 const MainContainer = () => {
     const movies = useSelector((store) => store?.movies?.nowPlayingMovies);
 
-    if (!movies || movies.length === 0) {
-        // Handle the case where movies is undefined, null, or an empty array
-        return <div className="text-white">No movies available</div>;
-    }
+    if(movies === null) return;
 
     const mainMovie = movies[0];
 
-    const { original_title, overview, id } = mainMovie || {};
+    const { original_title, overview, id } = mainMovie ;
 
     return (
         <div className="md:p-0 pt-[45%] bg-black">
